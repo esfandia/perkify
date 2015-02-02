f5c4c35df78e89a901c458fdd076dba599b97da4
Babak Esfandiari <babak@sce.carleton.ca>
2015-02-02 03:03:48 -0500

    added dependency and bidirectional association between perk and product

--- app/models/perk.rb
+++ app/models/perk.rb
@@ -1,4 +1,4 @@
 class Perk < ActiveRecord::Base
-  belongs_to :product
+  belongs_to :product, inverse_of: :perks
   validates :product, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
-  has_many :perks
+  has_many :perks, dependent: :destroy, inverse_of: :product
 end
